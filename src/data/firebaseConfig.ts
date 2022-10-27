a80397c201c08205c63876018f5904959b79641f
mpoweredo <mpow3r1337@gmail.com>
2022-10-27 21:11:37 +0200

    Installed and added firebase :heavy_plus_sign:

---
+++ src/data/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore();
+
+export { app, auth, db };
