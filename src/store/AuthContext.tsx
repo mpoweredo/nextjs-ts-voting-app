832a7eaf93efe93a020df4942d8064b22e05eaa5
mpoweredo <mpow3r1337@gmail.com>
2022-10-30 13:43:57 +0100

    Added sign in logic :sparkles:

--- src/store/AuthContext.tsx
+++ src/store/AuthContext.tsx
@@ -1,44 +1,60 @@
-import { auth, db } from 'data/firebaseConfig';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { doc, setDoc } from 'firebase/firestore';
-import { createContext, PropsWithChildren, useContext, useState } from 'react';
-import { IAuthContext } from 'types/authContext';
+import { auth, db } from 'data/firebaseConfig'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
+import { createContext, PropsWithChildren, useContext, useState } from 'react'
+import { IAuthContext } from 'types/authContext'
 
-const UserContext = createContext<IAuthContext | false>(false);
+const UserContext = createContext<IAuthContext | false>(false)
 
 export const AuthContextProvider = ({ children }: PropsWithChildren) => {
-	const [user, setUser] = useState();
+  const [user, setUser] = useState()
 
-	const signUp = async (email: string, password: string, name: string) => {
-		try {
-			const response = await createUserWithEmailAndPassword(auth, email, password);
-			const docRef = await doc(db, 'users', response.user.uid);
-			await setDoc(docRef, {
-				email,
-				name,
-			});
+  const signUp = async (email: string, password: string, name: string) => {
+    try {
+      const response = await createUserWithEmailAndPassword(auth, email, password)
+      const docRef = await doc(db, 'users', response.user.uid)
+      await setDoc(docRef, {
+        email,
+        name,
+      })
 
-			return "We've created your account for you.";
-		} catch (e: any) {
-			if (e instanceof Error) {
-				if (e.message === 'Firebase: Error (auth/email-already-in-use).') {
-					throw new Error('Email taken!');
-				}
-				throw new Error('Something went wrong... Try again later!');
-			}
-		}
-	};
+      return "We've created your account for you."
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        if (e.message === 'Firebase: Error (auth/email-already-in-use).') {
+          throw new Error('Email taken!')
+        }
+        throw new Error('Something went wrong... Try again later!')
+      }
+    }
+  }
 
-	return (
-		<UserContext.Provider
-			value={{
-				signUp,
-			}}>
-			{children}
-		</UserContext.Provider>
-	);
-};
+  const signIn = async (email: string, password: string) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+      return 'Successfully logged in!'
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        if (e.message === 'Firebase: Error (auth/wrong-password).') {
+          throw new Error('Credentials which You have passed are invalid!')
+        }
+        throw new Error('Something went wrong... Try again later!')
+      }
+    }
+  }
+
+  return (
+    <UserContext.Provider
+      value={{
+        signUp,
+        signIn,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
 
 export const UserAuth = () => {
-	return useContext(UserContext) as IAuthContext;
-};
+  return useContext(UserContext) as IAuthContext
+}
