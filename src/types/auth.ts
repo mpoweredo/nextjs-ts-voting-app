b8951db67cc4298450dd42d01b5c89c2cd7b15a9
mpoweredo <mpow3r1337@gmail.com>
2022-10-31 18:07:41 +0100

    Updated auth types :label:

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,10 +1,18 @@
+import { User as FirebaseUser } from 'firebase/auth'
+
 export interface ISignInValues {
-	email: string;
-	password: string;
+  email: string
+  password: string
 }
 
 export interface ISignUpValues {
-	name: string;
-	email: string;
-	password: string;
+  name: string
+  email: string
+  password: string
 }
+
+export interface IUserData {
+	name: string
+}
+
+export interface IUser extends FirebaseUser, IUserData {}
--- src/types/authContext.ts
+++ src/types/authContext.ts
@@ -1,3 +1,8 @@
+import { IUser } from "./auth"
+
 export interface IAuthContext {
-	signUp: (email: string, password: string, name: string) => Promise<string | undefined>;
+  signUp: (email: string, password: string, name: string) => Promise<string | undefined>
+  signIn: (email: string, password: string) => Promise<string | undefined>
+  isLoading: boolean
+  user: IUser | null
 }
