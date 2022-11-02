e75ef5de9c92184764c1bf098eeca7ff3d73fa29
mpoweredo <mpow3r1337@gmail.com>
2022-11-02 21:20:18 +0100

    Added app layout component :lipstick:

---
+++ src/components/layout/AppLayout.tsx
@@ -0,0 +1,14 @@
+import { Container, Stack } from '@chakra-ui/react'
+import React, { PropsWithChildren } from 'react'
+
+const AppLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <Container maxW={'container.lg'} minHeight="100vh" pt={88} display="flex" flexDir="column">
+      <Stack bg="blackAlpha.300" p={4} w="full" flex={1} mb={3} rounded="md">
+        {children}
+      </Stack>
+    </Container>
+  )
+}
+
+export default AppLayout
