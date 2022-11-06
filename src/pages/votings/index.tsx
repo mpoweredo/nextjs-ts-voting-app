6031d2cf2789a86c97980a297ade87ea728d4b60
mpoweredo <mpow3r1337@gmail.com>
2022-11-06 12:25:21 +0100

    Added votings page :sparkles:

---
+++ src/pages/votings/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import VotingsList from 'src/components/votings/VotingsList'
+
+const VotingsPage: NextPage = () => {
+  return <VotingsList />
+}
+
+export default VotingsPage
